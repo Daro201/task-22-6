35d30d5f9d926189bea0e044ab7b8079890b09ab
Adrian Meller <melleradria@gmail.com>
2018-09-07 18:51:47 +0200

    Add files

---
+++ client/modules/Post/components/PostListItem/PostListItem.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router';
+import { FormattedMessage } from 'react-intl';
+
+// Import Style
+import styles from './PostListItem.css';
+
+function PostListItem(props) {
+  return (
+    <div className={styles['single-post']}>
+      <h3 className={styles['post-title']}>
+        <Link to={`/posts/${props.post.slug}-${props.post.cuid}`} >
+          {props.post.title}
+        </Link>
+      </h3>
+      <p className={styles['author-name']}><FormattedMessage id="by" /> {props.post.name}</p>
+      <p className={styles['post-desc']}>{props.post.content}</p>
+      <div className={styles['post-action']}>
+        <a href="#" onClick={props.onDelete}><FormattedMessage id="deletePost" /></a>
+        <button className={styles['thumb-up-btn']} onClick={props.thumbUp}>+</button>
+        <span className={styles['votes-number']}>{props.post.voteCount}</span>
+        <button className={styles['thumb-down-btn']} onClick={props.thumbDown}>-</button>
+      </div>
+      <hr className={styles.divider} />
+    </div>
+  );
+}
+
+PostListItem.propTypes = {
+  post: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    slug: PropTypes.string.isRequired,
+    cuid: PropTypes.string.isRequired,
+  }).isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
+
+export default PostListItem;
